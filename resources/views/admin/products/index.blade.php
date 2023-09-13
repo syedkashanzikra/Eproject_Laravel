2e748c7c39ac11dc3d509987d48bf76dbda54a26
fareedmunawar <123103995+fareedmunawar@users.noreply.github.com>
2023-09-13 16:15:05 +0500

    jj

---
+++ resources/views/admin/products/index.blade.php
@@ -0,0 +1,31 @@
+<!-- resources/views/admin/products/index.blade.php -->
+
+@extends('layouts.admin.header-footer')
+
+@section('content')
+<div class="container">
+    <h1>Product List</h1>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Product Code</th>
+                <th>Product Name</th>
+                <th>Product Details</th>
+                <th>Product Image</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($products as $product)
+            <tr>
+                <td>{{ $product->product_code }}</td>
+                <td>{{ $product->product_name }}</td>
+                <td>{{ $product->product_details }}</td>
+                <td>
+                    <img src="{{ asset('path/to/your/images/' . $product->product_image) }}" alt="{{ $product->product_name }}" width="100">
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+@endsection
