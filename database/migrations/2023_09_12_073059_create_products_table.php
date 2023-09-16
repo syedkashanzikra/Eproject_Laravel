3599255fa7f34268eb4815df56345b7de82904a2
fareedmunawar <123103995+fareedmunawar@users.noreply.github.com>
2023-09-16 20:20:14 +0500

    ..

--- database/migrations/2023_09_12_073059_create_products_table.php
+++ database/migrations/2023_09_12_073059_create_products_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
         $table->string('product_name');
         $table->text('product_details');
         $table->string('product_image', 300);
-        $table->string('status');
         $table->timestamps();
     });
 }
